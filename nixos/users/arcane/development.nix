65cfd484b4bfdff59de0e7fa95d462c3e06f5d1d
arcanexis <3526162625@qq.com>
2025-12-05 19:56:08 +0800

    使用用户级nix配置持久化zsh配置

--- nixos/users/arcane/development.nix
+++ nixos/users/arcane/development.nix
@@ -1,5 +1,5 @@
 #  Arcane 开发环境配置
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   home.packages = with pkgs; [
@@ -43,12 +43,110 @@
 
   # 关键：即使系统安装了 zsh，这里也必须启用
   # 这样 Home Manager 才有权限去管理 ~/.zshrc 并写入 direnv 的 hook
-  programs.zsh = {
+   programs.zsh = {
     enable = true;
-    # 你可以在这里继续添加用户级的 zsh 配置，比如 alias, plugins 等
-    # shellAliases = {
-    #   ll = "ls -l";
-    # };
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+    };
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+
+    # 2. 使用新的 initContent API，并利用 mkMerge 保持顺序
+    initContent = lib.mkMerge [
+      (lib.mkBefore ''
+        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+          source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+        fi
+      '')
+
+      ''
+        # 加载 p10k 配置
+        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+        # LM Studio
+        export PATH="$PATH:/home/arcane/.lmstudio/bin"
+
+        # --- 自定义补全函数 ---
+        
+        # NixOS Rebuild 补全
+        if [ -n "''${commands[nixos-rebuild]}" ]; then
+          _nixos-rebuild() {
+            local curcontext="$curcontext" state line
+            typeset -A opt_args
+            _arguments -C '1: :->command' '*: :->args'
+            case $state in
+              (command)
+                local -a subcommands
+                subcommands=(
+                  "switch:build, activate and generation"
+                  "boot:build and add to boot menu"
+                  "test:build and activate but don't add to generation"
+                  "build:build but don't make active"
+                  "dry-activate:show what would be activated"
+                  "build-vm:build a virtual machine"
+                  "build-vm-with-bootloader:build a virtual machine with boot loader"
+                )
+                _describe -t commands 'nixos-rebuild command' subcommands
+                ;;
+            esac
+          }
+          compdef _nixos-rebuild nixos-rebuild
+        fi
+
+        # Flake 补全
+        _nix_flake() {
+          local curcontext="$curcontext" state line
+          typeset -A opt_args
+          _arguments -C '1: :->command' '*: :->args'
+          case $state in
+            (command)
+              local -a subcommands
+              subcommands=(
+                "run:run a flake app"
+                "develop:run a bash shell with the flake development environment"
+                "build:build a flake output"
+                "check:build and run checks"
+                "search:search packages"
+                "registry:manage flake registry"
+                "update:update flake lock file"
+                "init:initialize a flake in the current directory"
+                "show:show the outputs of a flake"
+                "clone:clone a flake into a directory"
+                "archive:create a tarball from a flake"
+                "bundles:manage bundles"
+                "copy:copy flake outputs to a store"
+                "edit:open the flake in $EDITOR"
+                "eval:evaluate a nix expression"
+                "log:show the build log of a flake output"
+                "path-info:query information about store paths"
+                "repl:start an interactive environment for evaluating Nix expressions"
+                "profile:manage Nix profiles"
+              )
+              _describe -t commands 'nix flake command' subcommands
+              ;;
+            (args)
+              case $words[2] in
+                (build|develop|run|show|check|archive)
+                  _files
+                  ;;
+              esac
+              ;;
+          esac
+        }
+        compdef _nix_flake nix
+      ''
+    ];
   };
 
 }
